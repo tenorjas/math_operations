0ff30fd866cb0364747e218bd2ea9bec7d838d55
Jonathan Schultz <jonathan.a.schultz@gmail.com>
2017-08-14 16:22:03 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace math_operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
